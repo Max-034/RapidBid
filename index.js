c7fef4b17cae49d680158e6c1e15ff0d818b9f95
Max-034 <148268311+Max-034@users.noreply.github.com>
2024-05-30 14:41:53 +0530

    trades setup /sockets set up/ frontend integrated

--- index.js
+++ index.js
@@ -3,40 +3,155 @@ import { createClient } from '@supabase/supabase-js'
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt";
 import session from "express-session";
-import passport from "passport";
-import { Strategy } from "passport-local";
 import multer from "multer";
 import { decode } from "base64-arraybuffer";
+import cors from "cors";
+import morgan from "morgan";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const supabase = createClient('https://qhryeiuegymozsriktoh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFocnllaXVlZ3ltb3pzcmlrdG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MzczODEsImV4cCI6MjAzMTExMzM4MX0.e-dm_pqJ6blrjPe8dTqtSuGv24cRnbA3aYfQtKt_N6E')
 const app = express();
+const server = createServer(app);
 const saltr = 10;
 
-app.use(session({
+const corop = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+
+app.use(cors(corop));
+
+const io = new Server(server , {
+  cors: {
+
+     origin: 'http://localhost:5173',
+    credentials: true
+
+  }
+});
+
+
+
+
+
+io.on('connection' , async (socket) => {
+
+
+let { data: trades, error } = await supabase
+  .from('trades')
+  .select('*')
+
+  //console.log(trades);
+
+  io.emit('chat' , 'hi')
+
+  io.emit('trades' , trades);
+
+ 
+
+  });
+
+   
+const handleInserts = async (payload) => {
+  // console.log('Change received!', payload)
+  let { data: trades, error } = await supabase
+  .from('trades')
+  .select('*')
+
+  console.log(trades);
+
+  io.emit('trades' , trades);
+  
+}
+
+
+supabase
+  .channel('trades')
+  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'trades' }, handleInserts)
+  .subscribe()
+
+
+
+
+
+// Middleware
+
+const sessionmiddleware = session({
     secret: "parth",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: false,
+    } , 
+  
+});
 
 
-}));
+
+app.use(sessionmiddleware);
+
+function isAuthenticated (req, res, next) {
+  if (req.session.user) {console.log("oh"); next(); }
+  else {res.send("chal hat")}
+}
+
+// io.engine.use(sessionmiddleware);
+
+app.use(bodyParser.json({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(morgan('combined'));
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.use(express.static("public"));
 
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.post("/register" , async (req,res) =>
+
+app.get('/' , (req,res) => {
+  res.send("okay");
+})
+
+app.get('/trade' , async (req,res) => 
 {
+  let { data: trades, error } = await supabase
+  .from('trades')
+  .select('*')
+
+  res.send(trades);
+
+})
+
+
+
+
+app.post("/register" , cors(corop) ,  async (req,res) =>
+{
+    try
+    {
     const user = req.body.username;
     const pass = req.body.password;
+
+     const data = await supabase
+        .from('Users')
+        .select()
+        .eq('username', user);
+
+        console.log(data);
+
+    if(data.data.length > 0)
+    {
+       res.send("fail");
+    }    
+    else{
 
     bcrypt.hash(pass, saltr, async function(err, hash) {
         const { data, error } = await supabase
@@ -46,33 +161,55 @@ app.post("/register" , async (req,res) =>
     ])
     .select()
 
-    var userr = await supabase
-    .from('Users')
-    .select()
-    .eq('username', user);
+    res.send("success");
 
-    userr = userr.data[0];
 
-    req.login(userr, (err) => {
-      console.log("success");
-      res.redirect("/register");
-    });
 
     });
+
+    }
+
+}
+catch(err)
+{
+   
+}
    
     
-console.log(user);
+
 
 })
 
+app.get("/axios" , async(req,res) => {
+
+  //   const { data} = supabase
+  // .storage
+  // .from('image')
+  // .getPublicUrl('image/front/item0/item.png');
+
+  // res.send(data);
+  // console.log((req.session.user.items));
+
+  // req.session.user.items= req.session.user.items++;
+
+  // console.log(req.session.user.items);
+  
+
+
+console.log(req.session.user)
+res.send("theek");
+  
+
+})
+
+
+
+
+
+
 app.get("/register" , async (req,res) =>
 {
-    if (req.isAuthenticated()){
-        res.send(req.user);
-    }
-    else{
-        res.send("ge");
-    }
+  res.send("hm");
 })
 
 const array = [
@@ -80,60 +217,51 @@ const array = [
     {name: 'item' , maxCount: 1}
 ]
 
-// app.post("/test" , upload.fields(array), async (req, res, next) =>
-//     {
-//         const filee = req.files.item[0];
-
-//         const fileBase64 = decode(filee.buffer.toString("base64"));
-        
-//         const { data, error } = await supabase
-//   .storage
-//   .from('image')
-//   .upload( 'item1/f.png' , fileBase64, {
-//     contentType: "image/png",
-//   })
 
 
-
-  
-
-//   res.send(user.password);
-
-
-  
-// })
-
-
-app.post("/login",
-    passport.authenticate("local", {
-      successRedirect: "/register",
-      failureRedirect: "/login",
-    })
-  );
-
-
-  app.post("/new-item" , upload.fields(array) , async(req , res , next) =>
+  app.post("/new-item" , isAuthenticated , upload.fields(array) , async(req , res , next) =>
 {
-    if(req.isAuthenticated())
-        {
+
     const file = req.files.item[0];
     const fileBase64 = decode(file.buffer.toString("base64"));
-    const { data, error } = await supabase.storage.from('image').upload( '/' + req.user.username + '/item' + req.user.items + '/item.png' , fileBase64, {contentType: "image/png",})
-    await supabase.storage.from('image').upload( '/' + req.user.username + '/item' + req.user.items +'/desc.txt'  , req.body.desc);
-    await supabase.storage.from('image').upload( '/' + req.user.username + '/item' + req.user.items +'/name.txt'  , req.body.name);
-    await supabase.storage.from('image').upload( '/' + req.user.username + '/item' + req.user.items +'/basep.txt'  , req.body.basep);
+    await supabase.storage.from('image').upload( '/' + req.session.user.username + '/item' + req.session.user.items + '/item.png' , fileBase64, {contentType: "image/png",})
 
 
-    await supabase.from('Users').update({ items: req.user.items++ }).eq('username', req.user.username).select()
+    const { data } = supabase
+  .storage
+  .from('image')
+  .getPublicUrl('/' + req.session.user.username + '/item' + req.session.user.items + '/item.png');
+
+  
+   await supabase
+  .from('trades')
+  .insert([
+    { path: data.publicUrl, desc: req.body.desc , basep: req.body.basep , name: req.body.name },
+  ])
+  .select()
+
+
+    await supabase.from('Users').update({ items: req.session.user.items + 1 }).eq('username', req.session.user.username).select();
+    req.session.user.items = req.session.user.items + 1;
+    console.log(req.session.user.items);
+
+    res.send("bhaiyar")
+        
+    
         
 
-    res.send(req.files.item[0])
-        }
-        else
-        {
-            res.redirect('/login')
-        }
+})
 
+app.get('/auth' , async(req , res) => {
+
+  console.log(req.session);
+  if(req.session.user)
+  {
+    res.send({user: req.session.user});
+  }
+  else{
+     res.send("fail")
+  }
 })
 
 
@@ -142,9 +270,16 @@ app.post("/login",
 
 
 
-  passport.use(
-    new Strategy(async function verify(username, password, cb) {
-      try {
+        app.post('/login' , async(req,res) => {
+
+          req.session.regenerate(function (err) {
+          if (err) next(err)
+          })
+
+          const {username , password} = req.body;
+
+
+          console.log(username);
         const result = await supabase
         .from('Users')
         .select()
@@ -156,32 +291,31 @@ app.post("/login",
           bcrypt.compare(password, storedHashedPassword, (err, valid) => {
             if (err) {
               console.error("Error comparing passwords:", err);
-              return cb(err);
+              res.redirect('/axios');
             } else {
               if (valid) {
-                return cb(null, user);
+                req.session.user = user;
+                req.session.save(function(err) {
+  // session saved
+})
+                
+                console.log("hmm");
+                res.send(req.session);
               } else {
-                return cb(null, false);
+                console.log("wtf");
+                res.send(req.session);
               }
             }
           });
         } else {
-          return cb("User not found");
+          console.log("bhag be");
+          res.redirect('/axios');
         }
-      } catch (err) {
-        console.log(err);
-      }
-    })
-  );
-  
-  passport.serializeUser((user, cb) => {
-    cb(null, user);
-  });
-  passport.deserializeUser((user, cb) => {
-    cb(null, user);
-  });
 
-app.listen(3000 , () => 
+        })
+       
+
+server.listen(3000 , () => 
 {
     console.log("o7");
 })
